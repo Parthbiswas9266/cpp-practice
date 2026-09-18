2b4feb0753ac2979ba1b5202d3a1663df31f6296
Parth Biswas <parthbiswas6262@gmail.com>
2026-09-18 21:38:27 +0530

    Update 01_BasicAdditionFunctions.cpp
    using functions to add numbers

--- 2. Functions/01_BasicAdditionFunctions.cpp
+++ 2. Functions/01_BasicAdditionFunctions.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+
+
+int add(){
+    int a, b;
+    cout<<"Enter a - "<<endl;
+    cin>>a;
+    cout<<"Enter b - "<<endl;
+    cin>>b;
+    return a + b;
+}
+
+int main() {
+ cout<<"Total = "<<add()<<endl;
+ return 0;
+}
